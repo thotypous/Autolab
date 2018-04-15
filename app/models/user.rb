75a39c816aff884baacf86ffa50867fc43b93c91
Paulo Matias <matias@ufscar.br>
2018-04-15 11:01:50 -0300

    Authenticate only through Shibboleth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+  devise :database_authenticatable,
+         :trackable, :validatable,
          :confirmable
 
   devise :omniauthable, omniauth_providers: [:shibboleth]
